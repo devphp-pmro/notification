cfa2a587c1831b6e03f3d7479e56c663503f3cbf
leo-pmro <46794609+leo-pmro@users.noreply.github.com>
2020-10-04 20:23:37 -0300

    novo

--- test/index.php
+++ test/index.php
@@ -2,10 +2,10 @@
 
     require_once __DIR__ . "/../lib_ext/autoload.php";
     
-    $mail =  new \Notification\Email(2,"smtp.gmail.com","devphp.pmro","devpmro2020","587","devphp.pmro@gmail.com","Dev PGO");
+    $mail =  new \Notification\Email(2,"","","","","","");
     $mail->sendMail("Assunto teste","<p>Este é um email tete</p>",
-      "devphp.pmro@gmail.com","Leonardo Magalhães",
-      "magalhaesro1979@gmail.com","Leonardo");
+      "","",
+      "","");
   
   
     
